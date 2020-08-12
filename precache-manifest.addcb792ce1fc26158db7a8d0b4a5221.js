89b698e14b3a708407745df8db4034a3d7b6b32d
almeidx <almeidx@pm.me>
2020-08-12 14:19:02 +0100

    Updates

---
+++ precache-manifest.addcb792ce1fc26158db7a8d0b4a5221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae82c28018e1d91afff62e1592b8043b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77be17dadc2354752548",
+    "url": "/static/js/2.6d81cc10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d81cc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c76f3d174f31929589",
+    "url": "/static/js/main.929ea273.chunk.js"
+  },
+  {
+    "revision": "8bf71d4cf8df14ee7b25",
+    "url": "/static/js/runtime-main.1b3c0a0b.js"
+  }
+]);
